912c72d7b72b6726cb4f0e3a6e77e78232a2e368
Max Safo <maxsafo@Maxs-MBP.fios-router.home>
2021-04-08 01:16:09 -0400

    Tests

--- VMI/main.py
+++ VMI/main.py
@@ -5,17 +5,20 @@ Ui_MainWindow, QtBaseClass = uic.loadUiType("VMI_GUI.ui")
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
-    def __init__(self, parent=None):
-        QtWidgets.QMainWindow.__init__(self, parent=parent)
+    def __init__(self):
+        super(MainWindow, self).__init__()
         self.setupUi(self)
         menu_bar = self.menuBar()
         menu_bar.setNativeMenuBar(False)
+        self.viewer = GraphicsWindow(self)
 
-        self.graphics()
 
-    def graphics (self):
-        scene = QtWidgets.QGraphicsScene()
-        self.graphicsView.setScene(scene)
+class GraphicsWindow(QtWidgets.QGraphicsView):
+    def __init__(self, parent):
+        super(GraphicsWindow, self).__init__(parent)
+        self._scene = QtWidgets.QGraphicsScene(self)
+        self.graphicsView.setScene(self._scene)
+        self.setScene(self._scene)
         pen = QtGui.QPen(QtCore.Qt.green)
         side = 20
 
@@ -23,6 +26,19 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             for j in range(100):
                 r = QtCore.QRectF(QtCore.QPointF(i * side, j * side), QtCore.QSizeF(side, side))
                 scene.addRect(r, pen)
+                r = print([j])
+
+    def wheelEvent(self, event):
+        if event.angleDelta().y() > 0:
+            factor = 1.25
+            self._zoom += 1
+        else:
+            factor = 0.8
+            self._zoom -= 1
+        if self._zoom > 0:
+            self.scale(factor, factor)
+        else:
+            self._zoom = 0
 
 
 if __name__ == '__main__':
