277d361f0c12f20e388daa6b9b9f27a0cafbc63b
perezfg <perezfg@vcu.edu>
2021-06-09 17:20:11 -0700

    Update .mem and .vmem

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class MainWindow(QWidget, Ui_MainWindow):
         self.show()
 
     def browser_file(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open File', 'C:/', 'Dumps (*.vmem, *.mem)')
+        fname = QFileDialog.getOpenFileName(self, 'Open File', 'C:/', 'Dumps (*.mem *.vmem)')
         self.lineEdit.setText(fname[0])
 
 
