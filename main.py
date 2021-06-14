06c1bb999ee59dfd7423c3c3c8442c61713070c8
Masrik <Masrik-Dahir@github.com>
2021-06-13 22:45:19 -0400

    Show the processes in the graphicView()

--- main.py
+++ main.py
@@ -13,12 +13,14 @@ class MainWindow(QWidget, Ui_MainWindow):
         super(MainWindow, self).__init__(parent)
         uic.loadUi("form.ui", self)
         self.pushButton.clicked.connect(self.browser_file)
-        self.pushButton_3.clicked.connect(self.create_process)
+        self.pushButton_3.clicked.connect(self.dropdown_selection)
+        # self.graphicsView
         self.show()
 
     def browser_file(self):
         self.fname = QFileDialog.getOpenFileName(self, 'Open File', 'C:/', 'Dumps (*.mem *.vmem)')
         self.lineEdit.setText(self.fname[0])
+
         print(self.fname[0])
         location = 'python2.7 volatility/vol.py imageinfo -f ' + self.fname[0]
         p = os.popen(location).read()
@@ -47,10 +49,40 @@ class MainWindow(QWidget, Ui_MainWindow):
             print(string)
 
 
-    def create_process(self):
-        print("Button is clicked!")
-        a = self.comboBox.itemText()
-        print(a)
+    def dropdown_selection(self):
+        a = self.comboBox.currentText()
+        if (str(a) == 'Observe'):
+            location = 'python2.7 volatility/vol.py imageinfo -f ' + self.fname[0]
+            p = os.popen(location).read()
+            print(p)
+            splitted = p.split('\n', 1)
+            profiles = splitted[0].strip()
+            print(profiles)
+            profiles = profiles.split(',')
+            print(profiles)
+            # index = profiles[0].find("Win")
+            # print(index)
+            i = 0
+            while i < len(profiles):
+                index = profiles[0].find("Win")
+                suggested = profiles[0][index:]
+                profiles[0] = suggested.strip()
+                i += 1
+            print(profiles)
+            first = profiles[0]
+            pslist = 'python2.7 volatility/vol.py --profile=' + first + ' pslist -f ' + self.fname[0]
+            p = os.popen(pslist).read()
+            print(p)
+            splitted = p.split('\n', 2)
+            processes = splitted[2].split('\n')
+
+            for string in processes:
+                self.graphicsView.scene().addText(string)
+                print(string)
+            self.graphicsView.show()
+
+        if (str(a) == 'Modify'):
+            print(a)
 
 app = QtWidgets.QApplication(sys.argv)
 window = MainWindow()
